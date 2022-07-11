6b39f1dd8cf5a87c5a7c51c3d87696773b05a6e7
Dakshina Palasamudrum <dakshinapalas@Dakshinas-MacBook-Pro.local>
2022-07-10 21:20:06 -0700

    seeing if changes work

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -7,7 +7,7 @@ function MainPage() {
     <div class="main-container">
       <video src="/videos/video-1.mp4" autoPlay loop muted />
       <h1>HELLO</h1>
-      <p>Welcome to my Universe</p>
+      <p>Welcome to my website!</p>
     </div>
   );
 }
