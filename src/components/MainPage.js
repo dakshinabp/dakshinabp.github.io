80d023e699bc40c4b1e64e4cc68584324c0b02f1
Dakshina Palasamudrum <dakshinapalas@Dakshinas-MacBook-Pro.local>
2022-10-08 16:58:22 -0400

    about page cards

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "../App.css";
 import "./MainPage.css";
+import Image from "./../assets/images/me.JPG";
 
 function MainPage() {
   return (
     <div class="main-container">
-      {/* <video src="/videos/video-1.mp4" autoPlay loop muted /> */}
-      <h1>HELLO,</h1>
-      <p>I'm Dakshina</p>
+      <img src= {Image} width="250" alt="dakshina" />
+      <div class="text-home">
+        <h1>HELLO,</h1>
+        <p>I'm Dakshina</p>
+      </div>
     </div>
   );
 }
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,46 +1,40 @@
 import React from "react";
 import "../../App.css";
-import Image from "../../assets/images/me.JPG";
+import Image from "../../assets/images/mlh.png";
+import Image2 from "../../assets/images/sparklist.webp";
+import Image3 from "../../assets/images/palas.jpg";
 
 export default function About() {
   return (
     <div class="about-container">
-      <img src={Image} width="300" alt="dakshina" />
-      <div class ="text">
-        <h1>About</h1>
-        <p class="about-bio">
-        Hey! I am Dakshina Palasamudrum.
-        I am a recent graduate from the University of California, Berkeley and 
-        studied Economics and Computer Science. My journey building things on the
-        internet started a few years ago when I took my first python course, ever 
-        since then, I have only continued developing my engineering skills. Other 
-        than tech, I am also passionate about fashion, travel, and trying out new 
-        places to eat!
-        </p>
-        <h1>Professional Bio</h1>
-        <p class="about-pro-bio">
-          My experience as a production engineering fellow with Meta via Major League 
-          Hacking, and personal projects have allowed me to actualize my passion for 
-          software development and scalability. I applied those skills when my team and 
-          I built a portfolio web app with user authentication using Flask, SQL, Linux, 
-          and bash scripting to support site reliability infrastructure. I improved my 
-          development skills by learning the fundamentals of AWS and Kubernetes, and working 
-          in an agile environment alongside Meta mentors and employees. Aside from the fellowship, 
-          I have developed and written code for personal projects, such as building a website using 
-          React, JavaScript, and CSS, as well as creating a “Celebrity Crush” game, focusing 
-          on the backend. During my time at Berkeley, I have also worked as a research intern for the undergraduate 
-          research apprentice program, marketing and design intern at Sparklist, consultant at
-          Navisens Technology, and interned for the World Financial Group. 
-          I learned firsthand how intertwined business and technology are when I
-          started my own clothing company in high school where I was able to gain
-          valuable skills such as leadership, and design via adobe photoshop and
-          web development via Squarespace. My experiences have allowed me to gain strong problem-solving and
-          communication skills as well as learning to work in a very fast-paced
-          environment.
-          
-        </p>
-
+      <div class ="card">
+        <img class="card-img" src= {Image} width="200" alt="mlh logo" />
+        <div class="text">
+          <h1>Meta (Major League Hacking Fellowship)</h1>
+          <p class="role">Production Engineering Fellow</p>
+          <p class="term">Summer 2021</p>
+        </div>
       </div>
+    
+      <div class ="card">
+        <img class="card-img" src= {Image2} width="50" height="300" alt="sparklist logo" />
+        <div class="text">
+          <h1>Sparklist</h1>
+          <p class="role">Design and Marketing Intern</p>
+          <p class="term">Feb. 2020 – May 2020</p>
+        </div>
+      </div>
+
+      <div class ="card">
+        <img class="card-img" src= {Image3} width="50" alt="palas logo" />
+        <div class="text">
+          <h1>Palas Designs</h1>
+          <p class="role">CEO and Founder</p>
+          <p class="term">Nov. 2016 – Sep. 2018</p>
+        </div>
+      </div>
+
+      
       
     </div>
   );
